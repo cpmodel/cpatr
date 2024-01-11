51fb76f0f7913418b086582ea7fd4658cb789557
ghardadi <gilang.hardadi@gmail.com>
2024-01-11 15:33:20 -0500

    Assign results folder of database conversion

--- R/data_conversion.R
+++ R/data_conversion.R
@@ -1,27 +1,21 @@
 library(cpatr)
 library(tidyverse)
-library(purrr)
-
-# Initializing the list
-UserScenario                  = list()
-
-# Parameters defining the carbon price trajectory
-UserScenario$CTintroYear      = 2030
-UserScenario$CTintroValue     = 20
-UserScenario$CTtargetYear     = 2040
-UserScenario$CTtargetValue    = 100
-
-# Fuel and sector coverage
-UserScenario$SelectedFuels    = c('nga', 'coa', 'oop', 'gso', 'die', 'lpg', 'ker')
-UserScenario$SelectedSectors  = c('res', 'foo', 'srv', 'mac')
 
 HistoricDataset = cpatr::DB_AllCountries
 FullBaseList = cpatr::BaseList_AllCountries
-CleanCountryList = c("USA", "ZAF")
-UserScen = UserScenario
 
-if (!file.exists("data_py")){
-  dir.create("data_py")
+# Edit the results pathway accordingly (directing to CPAT_Python repository)
+results_loc <- "C:/Users/wb582890/OneDrive - WBG/Documents/github/cpat_python/csv_data/"
+
+if (!file.exists(results_loc)){
+  dir.create(results_loc)
+}
+
+# Extracting DB_AllCountries
+conv_db_loc <- paste0(results_loc, "DB_AllCountries/")
+
+if (!file.exists(conv_db_loc)){
+  dir.create(conv_db_loc)
 }
 
 placeholder_id <- data.frame(HistoricDataset %>%
@@ -36,12 +30,16 @@ for (i in variables){
   tempdata <- cbind(placeholder_id, tempdata)
   assign(i, tempdata)
 
-  write.csv(get(i), paste0("data_py/", paste0(i,".csv")),
+  write.csv(get(i), paste0(conv_db_loc, paste0(i,".csv")),
             row.names=FALSE)
 }
 
+# Extracting BaseList_AllCountries
+conv_base_loc <- paste0(results_loc, "BaseList_AllCountries/")
 
-FullBaseList = cpatr::BaseList_AllCountries
+if (!file.exists(conv_base_loc)){
+  dir.create(conv_base_loc)
+}
 
 for (element in names(FullBaseList)) {
   if (!'list' %in% class(FullBaseList[[element]])){
@@ -53,7 +51,7 @@ for (element in names(FullBaseList)) {
       matrix_columns = sub(".*\\.", "", colnames(as.data.frame(FullBaseList[element])))
       temp <- as.data.frame(FullBaseList[element])
       colnames(temp) <- matrix_columns
-      write.csv(temp, file = paste0(element, ".csv"),
+      write.csv(temp, file = paste0(conv_base_loc, paste0(element, ".csv")),
                 row.names = FALSE)
     }
   } else {
@@ -63,8 +61,39 @@ for (element in names(FullBaseList)) {
         matrix_columns = sub(".*\\.", "", colnames(as.data.frame(FullBaseList[[element]][LU_element])))
         temp <- as.data.frame(FullBaseList[[element]][LU_element])
         colnames(temp) <- matrix_columns
-        write.csv(temp, file = paste0(element, "_", LU_element, ".csv"),
+        write.csv(temp, file = paste0(conv_base_loc, paste0(element, "_", LU_element, ".csv")),
                   row.names = FALSE)
     }
   }
 }
+
+conv_price_loc <- paste0(results_loc, "Price_AllCountries/")
+
+if (!file.exists(conv_price_loc)){
+  dir.create(conv_price_loc)
+}
+
+write_csv(RInputs_InternationalPrices_RegionMarket, paste0(conv_price_loc, "InternationalPrices_RegionMarket.csv"))
+write_csv(RInputs_InternationalPrices_IntPrices, paste0(conv_price_loc, "InternationalPrices_IntPrices.csv"))
+write_csv(RInputs_InternationalPrices_RegionAssumptions, paste0(conv_price_loc, "InternationalPrices_RegionAssumptions.csv"))
+
+conv_gdp_loc <- paste0(results_loc, "GDP_AllCountries/")
+
+if (!file.exists(conv_gdp_loc)){
+  dir.create(conv_gdp_loc)
+}
+
+write_csv(RInputs_RawGDPRelativeToBase, paste0(conv_gdp_loc, "RawGDPRelativeToBase.csv"))
+
+# Initializing the list
+UserScenario                  = list()
+
+# Parameters defining the carbon price trajectory
+UserScenario$CTintroYear      = 2030
+UserScenario$CTintroValue     = 20
+UserScenario$CTtargetYear     = 2040
+UserScenario$CTtargetValue    = 100
+
+# Fuel and sector coverage
+UserScenario$SelectedFuels    = c('nga', 'coa', 'oop', 'gso', 'die', 'lpg', 'ker')
+UserScenario$SelectedSectors  = c('res', 'foo', 'srv', 'mac')
